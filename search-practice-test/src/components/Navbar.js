12cbcb9a25219d01fd2981bd932db78f3061e67e
izayaaaaaaa <izaya.orihara0011@gmail.com>
2023-07-09 11:34:59 +0800

    (not yet functional) gpt equivalent of the full page w3schools navbar

--- search-practice-test/src/components/Navbar.js
+++ search-practice-test/src/components/Navbar.js
@@ -1,54 +1,58 @@
-// NavBar.js (React Component)
-import { useState } from "react";
-import '../styles/navbar.css';
+import React, { useState, useEffect, useRef } from 'react';
+import { Link } from 'react-router-dom';
+import '../styles/navbar.css'
 
-const NavBar = () => {
-  const [isNavExpanded, setIsNavExpanded] = useState(false);
+const Navbar = () => {
+  const [isNavOpen, setIsNavOpen] = useState(false);
+  const isNavOpenRef = useRef(isNavOpen);
+
+  const openNav = () => {
+    setIsNavOpen(true);
+  };
+
+  const closeNav = () => {
+    setIsNavOpen(false);
+  };
+
+  useEffect(() => {
+    isNavOpenRef.current = isNavOpen;
+  }, [isNavOpen]);
+
+  useEffect(() => {
+    const handleEscKey = (event) => {
+      if (event.key === 'Escape') {
+        closeNav();
+      }
+    };
+
+    const handleOutsideClick = (event) => {
+      if (isNavOpenRef.current && !event.target.closest('.overlay-content')) {
+        closeNav();
+      }
+    };
+
+    document.addEventListener('keydown', handleEscKey);
+    document.addEventListener('click', handleOutsideClick);
+
+    return () => {
+      document.removeEventListener('keydown', handleEscKey);
+      document.removeEventListener('click', handleOutsideClick);
+    };
+  }, []);
 
   return (
-    <nav className="navigation">
-      <a href="/" className="brand-name">
-        MacroSoft
-      </a>
-      <button
-        className="hamburger"
-        onClick={() => {
-          setIsNavExpanded(!isNavExpanded);
-        }}
-      >
-        {/* icon from Heroicons.com */}
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="h-5 w-5"
-          viewBox="0 0 20 20"
-          fill="white"
-        >
-          <path
-            fillRule="evenodd"
-            d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM9 15a1 1 0 011-1h6a1 1 0 110 2h-6a1 1 0 01-1-1z"
-            clipRule="evenodd"
-          />
-        </svg>
-      </button>
-      <div
-        className={
-          isNavExpanded ? "navigation-menu expanded" : "navigation-menu"
-        }
-      >
-        <ul>
-          <li>
-            <a href="/">Home</a>
-          </li>
-          <li>
-            <a href="/about">About</a>
-          </li>
-          <li>
-            <a href="/contact">Contact</a>
-          </li>
-        </ul>
+    <>
+      <div id="myNav" className={`overlay ${isNavOpen ? 'open' : ''}`}>
+        <button className="closebtn" onClick={closeNav}>&times;</button>
+        <div className="overlay-content">
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+          <Link to="/contact">Contact</Link>
+        </div>
       </div>
-    </nav>
+      <span className="open-button" onClick={openNav}>&#9776;</span>
+    </>
   );
 };
 
-export default NavBar;
+export default Navbar;
